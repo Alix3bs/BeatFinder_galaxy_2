e5f08655a97fa3f6e15c10e1930ee8cc7e9c1454
Alix3bs <alixconstantin128@gmail.com>
2025-11-02 09:49:16 -0500

    Initial Commit

---
+++ BeatFinder/BeatFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatFinderApp.swift
+//  BeatFinder
+//
+//  Created by Tracie Constantin on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BeatFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeatFinderTests/BeatFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeatFinderTests.swift
+//  BeatFinderTests
+//
+//  Created by Tracie Constantin on 11/2/25.
+//
+
+import Testing
+@testable import BeatFinder
+
+struct BeatFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
